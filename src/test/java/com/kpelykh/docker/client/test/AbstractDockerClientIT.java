da5295a8d8d20f74ecb91ea7b0eff9f819b567e8
Jacob Bay Hansen <jacob.bay.hansen@gmail.com>
2014-08-06 12:17:00 +0200

    Added a method to "cleverly" figure out the Docker url. It tries "-Ddocker.url" first, then "$DOCKER_HOST", then "http://localhost:4243" (the old Docker port), and finally "http://localhost:2375" (the new Docker port).

--- src/test/java/com/kpelykh/docker/client/test/AbstractDockerClientIT.java
+++ src/test/java/com/kpelykh/docker/client/test/AbstractDockerClientIT.java
@@ -2,7 +2,9 @@ package com.kpelykh.docker.client.test;
 
 import com.kpelykh.docker.client.DockerClient;
 import com.kpelykh.docker.client.DockerException;
+import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
+import org.apache.http.conn.HttpHostConnectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -10,14 +12,17 @@ import org.testng.ITestResult;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractDockerClientIT extends Assert {
-	
+
 	public static final Logger LOG = LoggerFactory
 			.getLogger(AbstractDockerClientIT.class);
-	
+
 	protected DockerClient dockerClient;
 
 	protected List<String> tmpImgs;
@@ -26,11 +31,12 @@ public abstract class AbstractDockerClientIT extends Assert {
 
 	public void beforeTest() throws DockerException {
 		LOG.info("======================= BEFORETEST =======================");
-		String url = System.getProperty("docker.url", "http://localhost:4243");
-		LOG.info("Connecting to Docker server at " + url);
-		dockerClient = new DockerClient(url);
 
-		LOG.info("Creating image 'busybox'");
+        dockerClient = createDockerClient();
+
+        LOG.info("Docker version " + dockerClient.version().getVersion());
+
+        LOG.info("Creating image 'busybox'");
 		// need to block until image is pulled completely
 		logResponseStream(dockerClient.pull("busybox"));
 
@@ -38,7 +44,48 @@ public abstract class AbstractDockerClientIT extends Assert {
 		LOG.info("======================= END OF BEFORETEST =======================\n\n");
 	}
 
-	public void afterTest() {
+    private DockerClient createDockerClient() throws DockerException {
+        if (System.getProperty("docker.url") != null) {
+            try {
+                LOG.info("docker.url " + System.getProperty("docker.url"));
+                return getDockerClient(URI.create(System.getProperty("docker.url")));
+            } catch (Exception e) {
+                throw new RuntimeException("Unable to use -Ddocker.url '" + System.getProperty("docker.url") + "'", e);
+            }
+        }
+
+        if (System.getenv("DOCKER_HOST") != null) {
+            try {
+                LOG.info("$DOCKER_HOST " + System.getenv("DOCKER_HOST"));
+                return getDockerClient(URI.create(System.getenv("DOCKER_HOST")));
+            } catch (Exception e) {
+                throw new RuntimeException("Unable to use $DOCKER_HOST '" + System.getenv("DOCKER_HOST") + "'", e);
+            }
+        }
+
+        try {
+            return getDockerClient(URI.create("http://localhost:4243"));
+        } catch (ClientHandlerException ignore) {
+        }
+
+        try {
+            return getDockerClient(URI.create("http://localhost:2375"));
+        } catch (ClientHandlerException ignore) {
+        }
+
+        throw new RuntimeException("Unable to detect Docker; please use $DOCKER_HOST, -Ddocker.url, or make sure the Docker deamon is running on either port 4243 or 2375");
+    }
+
+    private DockerClient getDockerClient(URI host) throws DockerException {
+        final String url = String.format("http://%s:%d", host.getHost(), host.getPort());
+
+        LOG.info("Connecting to Docker server at " + url);
+        DockerClient dockerClient = new DockerClient(url);
+        dockerClient.version();
+        return dockerClient;
+    }
+
+    public void afterTest() {
 		LOG.info("======================= END OF AFTERTEST =======================");
 	}
 
